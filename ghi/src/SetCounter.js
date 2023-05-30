0d9fde8a637284812cc83da0319a5b1ea53ecfbd
Filamer Doronio <filamerdoronio@gmail.com>
2023-05-29 21:57:11 -0700

    implemented functionality to increment, decrement, and set features

--- ghi/src/SetCounter.js
+++ ghi/src/SetCounter.js
@@ -1,13 +1,33 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
+import { set } from './actions'
+
+
+
 export default function SetCounter() {
+    const storeCount = useSelector((state) => state.count)
+    const [count, setCount] = useState(storeCount);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        setCount(storeCount)
+    }, [storeCount])
+
     return (
         <section className="controls">
             <form
                 onSubmit={(e) => {
                     e.preventDefault();
+                    dispatch(set(count))
                 }}
             >
                 <label htmlFor="set-to">Set Count</label>
-                <input id="set-to" type="number"/>
+                <input 
+                    id="set-to"
+                    type="number"
+                    value={count}
+                    onChange={(e) => setCount(e.target.value)}
+                />
                 <input type="submit"/>
             </form>
         </section>
